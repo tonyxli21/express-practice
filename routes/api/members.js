383b046bfd880ed6fc2f1aa02e66bcb2e3f5d275
Tony Li <tonyxli20@gmail.com>
2022-07-28 14:46:44 -0500

    fixed syntax error for put request

--- routes/api/members.js
+++ routes/api/members.js
@@ -45,37 +45,52 @@ router.post("/", (req, res) => {
   members.push(newMember);
   // Send response
   res.json(members);
+});
 
-  // Update Member
-  // Check to make sure there exists a member with specified id
-  router.put("/:id", (req, res) => {
-    // some() filters array, returns boolean
-    const found = members.some(member => member.id === parseInt(req.params.id));
+// Update Member
+// Check to make sure there exists a member with specified id
+router.put("/:id", (req, res) => {
+  // some() filters array, returns boolean
+  const found = members.some(member => member.id === parseInt(req.params.id));
 
-    if (found) {
-      // Get name and email from req.body
-      const updatedMember = req.body;
-      // Loop through current members to check which one matches the id
-      members.forEach(member => {
-        if (member.id === parseInt(req.params.id)) {
-          // User ternary operator to make sure either or both are valid inputs
-          member.name = updatedMember.name ? updatedMember.name : member.name;
-          member.email = updatedMember.email
-            ? updatedMember.email
-            : member.email;
+  if (found) {
+    // Get name and email from req.body
+    const updatedMember = req.body;
+    // Loop through current members to check which one matches the id
+    members.forEach(member => {
+      if (member.id === parseInt(req.params.id)) {
+        // User ternary operator to make sure either or both are valid inputs
+        member.name = updatedMember.name ? updatedMember.name : member.name;
+        member.email = updatedMember.email ? updatedMember.email : member.email;
 
-          // Send message and member itself
-          res.json({ msg: "Member updated", member });
-        }
-      });
-    } else {
-      // If there does not exist a member with that id
-      // Give 400 error (bad request)
-      res
-        .status(400)
-        .json({ msg: `No member with the id of ${req.params.id}` });
-    }
-  });
+        // Send message and member itself
+        res.json({ msg: "Member updated", member });
+      }
+    });
+  } else {
+    // If there does not exist a member with that id
+    // Give 400 error (bad request)
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
+// Delete Member
+router.delete("/:id", (req, res) => {
+  // some() filters array, returns boolean
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if (found) {
+    // Use request object to get member info
+    // Filter out member with specified id
+    res.json({
+      msg: "Member deleted",
+      members: members.filter(member => member.id !== parseInt(req.params.id))
+    });
+  } else {
+    // If there does not exist a member with that id
+    // Give 400 error (bad request)
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
 });
 
 module.exports = router;
